aadc8fab2ba1dfa4a78aa85ac27bb4a086ef3baa
Martin Kostov <centrino2duo@gmail.com>
2016-12-01 09:42:55 +0200

    cleanedup webproject

--- VitalbetSportsProvider.WebClient/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace VitalbetSportsProvider.WebClient.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- VitalbetSportsProvider.WebClient/Startup.cs
+++ VitalbetSportsProvider.WebClient/Startup.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Owin;
+﻿using Microsoft.Owin;
 using Owin;
 
 [assembly: OwinStartup(typeof(VitalbetSportsProvider.WebClient.Startup))]
@@ -12,7 +9,6 @@ namespace VitalbetSportsProvider.WebClient
     {
         public void Configuration(IAppBuilder app)
         {
-            ConfigureAuth(app);
         }
     }
 }
