e389a770ad9542c78767fdb643ee535ea853360d
Martin Kostov <centrino2duo@gmail.com>
2016-12-01 21:19:37 +0200

    added working react

---
+++ VitalbetSportsProvider.WebClient/Scripts/Views/Navigation.jsx
@@ -0,0 +1,18 @@
+﻿var Home = React.createClass({
+    render: function() {
+        return (
+            <div className="container">
+	            <div className="navbar-header">
+		            <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+		            <span className="icon-bar" />
+		            <span className="icon-bar" />
+		            <span className="icon-bar" />
+		            </button>
+		            <a className="navbar-brand" href="/">Vitalbet Feed</a>
+	            </div>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(React.createElement(Home, null), document.getElementById("react-navigation"));
